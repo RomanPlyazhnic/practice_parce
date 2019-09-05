9520ba914fcdc47cde59799679e6d051e3a41984
RomanPlyazhnic <romanplyazhnic@gmail.com>
2019-09-05 14:53:26 +0300

    Created test table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Test.create(name: 'plyazhnic', created_at: Date.current, updated_at: Date.current)
